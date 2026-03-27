c027af8d6d8dd678ed5e1ce76c3821fb740893a4
daihongli <1215765236@qq.com>
2026-03-27 19:07:51 +0800

    Reactor

--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -30,7 +30,13 @@ export default sidebar({
           link: "reactor/",
           children: [
             { text: "第一章 初识响应式编程", icon: "pen-to-square", link: "01-intro" },
-            { text: "第二章 Flux 与 Mono 基础", icon: "pen-to-square", link: "02-flux-mono" }
+            { text: "第二章 Flux 与 Mono 基础", icon: "pen-to-square", link: "02-flux-mono" },
+            { text: "第三章 订阅流程与生命周期", icon: "pen-to-square", link: "03-lifecycle" },
+            { text: "第四章 线程调度 Schedulers", icon: "pen-to-square", link: "04-schedulers" },
+            { text: "第五章 Sinks.Many - 手动控制数据流", icon: "pen-to-square", link: "05-sinks" },
+            { text: "第六章 第六章：错误处理与资源清理", icon: "pen-to-square", link: "06-error-handling" },
+            { text: "第七章 背压处理 - 让快慢匹配", icon: "pen-to-square", link: "07-backpressure" },
+            { text: "第八章 Spring 集成与项目实战", icon: "pen-to-square", link: "08-spring-integration" }
           ]
         },
       ],
